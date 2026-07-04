2c22d5ca6256ba457bae8b3ba5db07d92ca0fe06
Rosa Mo <rosaatcoding@gmail.com>
2026-07-05 00:19:59 +0200

    Add dependency to pom

--- app-prueba/src/main/java/com/codigofacilito/maven/App.java
+++ app-prueba/src/main/java/com/codigofacilito/maven/App.java
@@ -1,9 +1,11 @@
 package com.codigofacilito.maven;
 
+import org.apache.commons.collections4.Unmodifiable;
+
 /**
  * Hello world!
  */
-public class App {
+public class App implements Unmodifiable {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
